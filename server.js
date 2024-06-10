627327077806697c59a8d61a482d77c7f83e8e42
himanshu <himanshu991139@gmail.com>
2024-06-10 15:26:43 +0530

    test

--- server.js
+++ server.js
@@ -1,28 +1,8 @@
-const usb = require('usb');
-
-// List all connected USB devices
-const devices = usb.getDeviceList();
-console.log(devices,"devices")
-devices.forEach((device) => {
-  try {
-    device.open();  // Open the device to retrieve detailed information
-  } catch (error) {
-    console.error('Error opening device:', error);
-    return;
-  }
-
-  const deviceDescriptor = device.deviceDescriptor;
-console.log(deviceDescriptor,"deviceDescriptor")  
-  console.log(`Device: ${deviceDescriptor.iProduct}`);
-  console.log(`Vendor ID: ${deviceDescriptor.idVendor}`);
-  console.log(`Product ID: ${deviceDescriptor.idProduct}`);
-  console.log(`Manufacturer: ${deviceDescriptor.iManufacturer}`);
-  console.log(`Serial Number: ${deviceDescriptor.iSerialNumber}`);
-  console.log(`Device Class: ${deviceDescriptor.bDeviceClass}`);
-  console.log(`Device SubClass: ${deviceDescriptor.bDeviceSubClass}`);
-  console.log(`Device Protocol: ${deviceDescriptor.bDeviceProtocol}`);
-  console.log(`Max Packet Size: ${deviceDescriptor.bMaxPacketSize0}`);
-  console.log('-----------------------------------------');
-
-  device.close();  // Close the device after retrieving information
-});
+var HID = require("node-hid");
+var devices = HID.devicesAsync()
+  .then((devices) => {
+    console.log(devices);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
