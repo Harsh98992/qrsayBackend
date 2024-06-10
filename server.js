a14886d4f138eb867cf5ee3e6b6321ef5928d722
himanshu <himanshu991139@gmail.com>
2024-06-10 15:13:02 +0530

    changes

--- server.js
+++ server.js
@@ -1,36 +1,23 @@
-const escpos = require('escpos');
-// install escpos-usb adapter module manually
-escpos.USB = require('escpos-usb');
-// Select the adapter based on your printer type
-const device  = new escpos.USB();
-// const device  = new escpos.Network('localhost');
-// const device  = new escpos.Serial('/dev/usb/lp0');
- 
-const options = { encoding: "GB18030" /* default */ }
-// encoding is optional
- 
-const printer = new escpos.Printer(device, options);
- 
-device.open(function(error){
-  printer
-  .font('a')
-  .align('ct')
-  .style('bu')
-  .size(1, 1)
-  .text('The quick brown fox jumps over the lazy dog')
-  .text('敏捷的棕色狐狸跳过懒狗')
-  .barcode('1234567', 'EAN8')
-  .table(["One", "Two", "Three"])
-  .tableCustom(
-    [
-      { text:"Left", align:"LEFT", width:0.33, style: 'B' },
-      { text:"Center", align:"CENTER", width:0.33},
-      { text:"Right", align:"RIGHT", width:0.33 }
-    ],
-    { encoding: 'cp857', size: [1, 1] } // Optional
-  )
-  .qrimage('https://github.com/song940/node-escpos', function(err){
-    this.cut();
-    this.close();
-  });
+const usb = require('usb');
+
+// List all connected USB devices
+const devices = usb.getDeviceList();
+
+devices.forEach((device) => {
+  device.open();  // Open the device to retrieve detailed information
+
+  const deviceDescriptor = device.deviceDescriptor;
+
+  console.log(`Device: ${deviceDescriptor.iProduct}`);
+  console.log(`Vendor ID: ${deviceDescriptor.idVendor}`);
+  console.log(`Product ID: ${deviceDescriptor.idProduct}`);
+  console.log(`Manufacturer: ${deviceDescriptor.iManufacturer}`);
+  console.log(`Serial Number: ${deviceDescriptor.iSerialNumber}`);
+  console.log(`Device Class: ${deviceDescriptor.bDeviceClass}`);
+  console.log(`Device SubClass: ${deviceDescriptor.bDeviceSubClass}`);
+  console.log(`Device Protocol: ${deviceDescriptor.bDeviceProtocol}`);
+  console.log(`Max Packet Size: ${deviceDescriptor.bMaxPacketSize0}`);
+  console.log('-----------------------------------------');
+
+  device.close();  // Close the device after retrieving information
 });
