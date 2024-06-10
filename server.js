54adb25bbde4796e8fcb966598db8625cc94e86b
himanshu <himanshu991139@gmail.com>
2024-06-10 15:31:24 +0530

    fdsa

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var HID = require("node-hid");
+console.log("hellp");
 var devices = HID.devicesAsync()
   .then((devices) => {
     console.log(devices);
   })
   .catch((err) => {
     console.log(err);
+  })
+  .finally(() => {
+    console.log("done");
   });
